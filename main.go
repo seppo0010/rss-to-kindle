da1c63071ebaa48b7d684df0ffbcffbcddef0129
Yiu Man Chi <ymcatar@gmail.com>
2016-08-26 10:49:15 +0800

    update: load configuration from .env file

--- main.go
+++ main.go
@@ -2,42 +2,71 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"rss-to-kindle/content"
 	"rss-to-kindle/kindle"
+	"strings"
 
 	"github.com/fatih/color"
+	"github.com/joho/godotenv"
 )
 
-func printConfiguration(email string, links []string) {
-	color.New(color.FgCyan).Add(color.Underline).Println("Configuration")
-	fmt.Println("Email:\t" + email)
+type configuration struct {
+	server    string
+	port      string
+	fromEmail string
+	password  string
+	toEmail   string
+	links     []string
+}
 
-	if len(links) > 0 {
-		fmt.Printf("Links:")
-		for _, link := range links {
+func printConfiguration(conf configuration) {
+	color.New(color.FgCyan).Add(color.Underline).Println("Configuration")
+	fmt.Println("SERVER:\t" + conf.server)
+	fmt.Println("PORT:\t" + conf.port)
+	fmt.Println("FROM:\t" + conf.fromEmail)
+	fmt.Println("PWD:\t" + "[redacted]")
+	fmt.Println("TO:\t" + conf.toEmail)
+	if len(conf.links) > 0 {
+		fmt.Printf("LINKS:")
+		for _, link := range conf.links {
 			fmt.Println("\t* " + link)
 		}
 	}
 }
 
 func main() {
-	server := os.Args[1]
-	port := os.Args[2]
-	fromEmail := os.Args[3]
-	password := os.Args[4]
-	toEmail := os.Args[5]
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	links := os.Args[6:]
+	conf := configuration{
+		os.Getenv("SERVER"),
+		os.Getenv("PORT"),
+		os.Getenv("FROM_EMAIL"),
+		os.Getenv("PASSWORD"),
+		os.Getenv("TO_EMAIL"),
+		strings.Split(os.Getenv("LINKS"), ","),
+	}
 
-	printConfiguration(fromEmail, links)
+	printConfiguration(conf)
 
-	for _, link := range links {
+	for _, link := range conf.links {
 		feed := content.GetFeed(link)
 		dir := content.MakeMain(feed)
 
 		mobiPath := kindle.GenerateMobi(dir)
-		kindle.Send(server, port, fromEmail, password, toEmail, mobiPath)
+
+		kindle.Send(
+			conf.server,
+			conf.port,
+			conf.fromEmail,
+			conf.password,
+			conf.toEmail,
+			mobiPath,
+		)
+
 		kindle.Cleanup(dir)
 	}
 }
